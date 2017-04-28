7c69281ed9d49417d7442c91ff7c5583e0b5eb8c
Nurymka <nurim98@gmail.com>
2017-04-28 15:30:12 -0700

    feat: eject from create react app

--- bruin_meet/src/App.js
+++ bruin_meet/src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-import addIcon from './img/ic-add.svg';
-import './App.css';
+import addIcon from './img/ic-plus.png';
+import menuIcon from './img/ic-menu.png';
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="body">
         <div className="home-header">
-          <h1 className="home-logo">BM</h1>
-          <a className="btn-add">
-            <img src={addIcon} className="ic-add" />
+          <a className="btn-header">
+            <img src={menuIcon} className="ic-circular" />
           </a>
+          <p className="txt-header color-blue">Bruin Meet</p>
+          <a className="btn-header">
+            <img src={addIcon} className="ic-circular" />
+          </a>
+        </div>
+        <div className="search-header">
+          <p className="txt-header">I am looking for a study partner</p>
         </div>
       </div>
     );
