bfb0f2ab6f514e0a2ab8cc8f884f21f3bda7fdc5
Nurymka <nurim98@gmail.com>
2017-05-05 17:50:26 -0700

    fix: include missing npm packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,29 @@
 // require dependencies
 var path = require('path')
 var webpack = require('webpack')
-var autoprefixer = require('autoprefixer');
 var BundleTracker = require('webpack-bundle-tracker')
 
 module.exports = {
   // the base directory (abs path) for resolving the entry option
   context: __dirname,
-  entry: './assets/src/index',
+  entry: [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './bruin_meet_project/src/index.js',
+  ],
   output: {
     // compiled bundle to be stored here
     path: path.resolve('./assets/bundles/'),
     // naming convention webpack uses for our files
     filename: '[name]-[hash].js',
+    publicPath: 'http://localhost:3000/assets/bundles/',
   },
 
   plugins: [
     // tells webpack where to store data about our bundles
     new BundleTracker({filename: './webpack-stats.json'}),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(), // don't reload if there is an error
     // make jquery available in every module
     new webpack.ProvidePlugin({
       $: 'jquery',
@@ -31,17 +37,12 @@ module.exports = {
       // tells webpack to use following loaders on all .js and .jsx files
       {test: /\.jsx?$/,
         //make babel not able to transpile all files in node_modules -> too lonng time
-
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          // specify use of react code
-          presets: ['react']
-        },
+        loaders: ['react-hot-loader', 'babel-loader?presets[]=es2015,presets[]=stage-0,presets[]=react'],
       },
       {
         test: /\.css$/,
-        loader: 'style!css?importLoaders=1!postcss'
+        loader: 'style-loader!css-loader?importLoaders=1!postcss-loader'
       },
       {
         test: /\.png$/,
@@ -50,23 +51,10 @@ module.exports = {
       }
     ]
   },
-  // We use PostCSS for autoprefixing only.
-  postcss: function() {
-    return [
-      autoprefixer({
-        browsers: [
-          '>1%',
-          'last 4 versions',
-          'Firefox ESR',
-          'not ie < 9', // React doesn't support IE8 anyway
-        ]
-      }),
-    ];
-  },
   resolve: {
     // tell webpack where to look for modules
-    modulesDirectories: ['node_modules'],
+    modules: ['node_modules'],
     // extensions that should be used to resolve modules
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   }
 }
