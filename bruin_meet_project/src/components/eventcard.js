6ce06bec61e80a90d48f2e48b3a36ac0e20244a4
Nurymka <nurim98@gmail.com>
2017-05-05 17:08:50 -0700

    refactor(remove node_modules from repo and move src to bruin_meet_project):

---
+++ bruin_meet_project/src/components/eventcard.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import heartIcon from '../img/ic-heart.png';
+import peopleIcon from '../img/ic-people.png';
+import '../styles/events.css';
+
+class EventCard extends Component {
+  render() {
+    return (
+      <div className='event-card'>
+        <div className='event-side' />
+        <p className='event-title'>Silicon Valley Watch Party</p>
+        <p className='event-location'>at Fireside Lounge</p>
+        <p className='event-time'>25 May, 4:30 PMM</p>
+        <div className='event-footer'>
+          <div className='event-tags'>
+            <Tag />
+            <Tag />
+          </div>
+          <div className='event-details'>
+            <div className='event-detail'>
+              <img src={heartIcon} className='ic-heart' />
+              <p className='event-detail-txt'>11/15</p>
+            </div>
+            <div className='event-detail'>
+              <img src={peopleIcon} className='ic-people' />
+              <p className='event-detail-txt'>5</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Tag extends Component {
+  render() {
+    return (
+      <div className='event-tag'>
+        Netflix
+      </div>
+    )
+  }
+}
+
+export default EventCard;
