a018ae69f8864861b38a28415eaebc48ea55d39d
EC2 Default User <ec2-user@ip-172-31-12-195.us-west-1.compute.internal>
2017-05-07 07:24:42 +0000

    Accept connections from all hosts

--- bruin_meet_project/settings.py
+++ bruin_meet_project/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["172.31.12.195"]
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
