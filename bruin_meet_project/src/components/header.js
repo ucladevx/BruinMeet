996b0102e74c20953564dcbdf382a3d65a8a8bf1
Suyash Saxena <suyash2099@gmail.com>
2017-06-05 23:29:36 -0700

    logo

--- bruin_meet_project/src/components/header.js
+++ bruin_meet_project/src/components/header.js
@@ -4,6 +4,7 @@ import RotatingText from 'react-rotating-text';
 import addIcon from '../img/ic-plus.png';
 import menuIcon from '../img/ic-menu.png';
 import logo from '../img/logo.png';
+import logo2 from '../img/logo2.png';
 import searchIcon from '../img/search.png';
 import SearchBar from '../components/searchBar.js';
 import '../styles/header.css'
@@ -11,7 +12,7 @@ import '../styles/header.css'
 const Header = (props) => (
   <div className="search-header">
 
-    <img src={logo} className="ic-logo" />
+    <img src={logo2} className="ic-logo" />
     <SearchBar></SearchBar>  
     <input type="image" src={searchIcon} onClick={() => this.handleClick(!this.state.clicked)} className = "ic-search"/>
   
