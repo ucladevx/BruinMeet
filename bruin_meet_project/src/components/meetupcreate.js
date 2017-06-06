d9120c8103b3e2ccd628e4a21e2694aafa187b2a
eltonkl <eltonkl@users.noreply.github.com>
2017-06-05 19:47:08 -0700

    Change p's to inputs for meetup creation form

--- bruin_meet_project/src/components/meetupcreate.js
+++ bruin_meet_project/src/components/meetupcreate.js
@@ -23,39 +23,40 @@ class Create extends Component {
         return (
     <div className='background'>
     <div className='formContainer'>
-    <div className='sideBar' />
-    <div className='topDiv'>
-        <p className='c-title' contenteditable='true'>Silicon Valley Watch Party</p>
-    </div>
-    <div className='loc-div'>
-        <p className='time' contenteditable="true">Time</p>
-        <p className='location' contenteditable="true">Location</p>
-    </div>
-    <div className='desc'>
-        <p contenteditable="true">Dinesh gets back to the ground while Richard continues to explore his new project. Gavin tries to come out of the new problem he has gotten himself into and interestingly the chemistry between Richard and Monica takes a new turn.
-        </p>
-    </div>
-    <div className='tags'>
-         <p className='p-tag'>add; tags; like; this</p>
-         <p className='p-ppl'>15 ppl</p>
-    </div>
-    <div className='buttons'>
-        <label>
-            <input className="c-inputStyles social" type="" value="Social" onChange={(e)=> this.setState({ type: "social" })}/>
-            <input className="c-inputStyles active" type="" value="Active" onChange={(e)=> this.setState({ type: "active" })}/>
-            <input className="c-inputStyles food" type="" value="Food" onChange={(e)=> this.setState({ type: "food" })}/>
-            <input className="c-inputStyles study" type="" value="Study" onChange={(e)=> this.setState({ type: "study" })}/>
-            <input className="c-inputStyles random" type="" value="Random" onChange={(e)=> this.setState({ type: "random" })}/>
-        </label>
-    </div>
-    <div className='foot'>
-        <input className="inputStyles cre" type="submit" value="Create Meetup" />
-        <div className="user">
-            <img src={host} className='ic-heart-d' />
-            <p className='host'>Hosted by<br></br>Alberto Duncan</p>
+        <form>
+            <div className='sideBar' />
+            <div className='topDiv'>
+                <input type="text" name="title" className='c-title' value="Silicon Valley Watch Party"/>
+            </div>
+            <div className='loc-div'>
+                <input type="text" name="time" className='time' value="Time"/>
+                <input type="text" name="location" className='location' value="Location"/>
+            </div>
+            <div className='desc'>
+                <textarea>Dinesh gets back to the ground while Richard continues to explore his new project. Gavin tries to come out of the new problem he has gotten himself into and interestingly the chemistry between Richard and Monica takes a new turn.</textarea>
+            </div>
+            <div className='tags'>
+                <input type="text" name="tags" className='p-tag' value="add; tags; like; this"/>
+                <input type="text" name="ppl" className='p-ppl' value="15 ppl"/>
+            </div>
+            <div className='buttons'>
+                <label>
+                    <input className="c-inputStyles social" type="button" value="Social" onChange={(e)=> this.setState({ type: "social" })}/>
+                    <input className="c-inputStyles active" type="button" value="Active" onChange={(e)=> this.setState({ type: "active" })}/>
+                    <input className="c-inputStyles food" type="button" value="Food" onChange={(e)=> this.setState({ type: "food" })}/>
+                    <input className="c-inputStyles study" type="button" value="Study" onChange={(e)=> this.setState({ type: "study" })}/>
+                    <input className="c-inputStyles random" type="button" value="Random" onChange={(e)=> this.setState({ type: "random" })}/>
+                </label>
+            </div>
+            <div className='foot'>
+                <input className="inputStyles cre" type="submit" value="Create Meetup" />
+                <div className="user">
+                    <img src={host} className='ic-heart-d' />
+                    <p className='host'>Hosted by<br></br>Alberto Duncan</p>
+                </div>
+            </div>
+        </form>
         </div>
-    </div>
-    </div>
     </div>)
     }
 }
